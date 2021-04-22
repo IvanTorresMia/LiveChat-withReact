7c23df13515d92fcd65c91375150e26810c02f61
Ivan Torres <ivantorresmia27@gmail.com>
2021-04-22 13:26:06 -0700

    bare bones of dash board

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: '50px',
+        padding: theme.spacing(3, 2)
+    }
+}))
 
 
 const Dashboard = () => {
 
+    const classes = useStyles()
+
     return (
         <div>
-
+            <Paper className={classes.root}>
+                <Typography variant="h4" component="h4">
+                   Chat App
+                </Typography>
+                <Typography component="p">
+                    place holder
+                </Typography>
+            </Paper>
         </div>
     )
 }
